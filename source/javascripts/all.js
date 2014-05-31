a2707753a535c4c97ad07fef5ed7083c99b5cbc0
Julie Lesseg <jlesseg@gmail.com>
2014-05-31 14:11:15 -0700

    more starzz

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,12 +2,19 @@
 
 $(document).ready(function(){
 	
-	$(function(){
-      $("#slides").slidesjs({
-        width: 940,
-        height: 528
-      });
-    });
+	 $(function() {
+		$('#slides').slidesjs({
+		width: 940,
+		height: 528,
+		play: {
+		active: true,
+		auto: true,
+		interval: 4000,
+		swap: true
+		}
+		});
+		
+	}); 
 	
 	
 });
